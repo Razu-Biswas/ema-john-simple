e458c89299ce889c5faa03251b56027a5e7789cb
Razu-Biswas <sreekantaraju3421@gmail.com>
2021-03-08 23:22:21 +0600

    all ok

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Cart.css';
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -6,18 +8,18 @@ const Cart = (props) => {
     // const total = cart.reduce((total, prd) => total + prd.price, 0);
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
-        const product = cart[i];    
-        total = total + product.price;
+        const product = cart[i];
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
-    if(total > 35){
+    if (total > 35) {
         shipping = 0;
     }
-    else if(total > 15){
+    else if (total > 15) {
         shipping = 4.99;
     }
-    else if(total > 0){
+    else if (total > 0) {
         shipping = 12.99;
     }
 
@@ -29,14 +31,20 @@ const Cart = (props) => {
         return Number(precision);
     }
     return (
-        <div>
-            <h4>Order Summary</h4>
-            <p>Items ordered : $ {cart.length}</p>
+        <div className="cart-container">
+            <h4 className="text-danger">Order Summary</h4>
+            <p>Items Ordered :  {cart.reduce((total, quantity) => total + quantity.quantity,0)}</p>
             <p>Product Price: $ {formatNumber(total)}</p>
             <p><small>Shipping Cost: $ {shipping}</small></p>
             <p><small>Tax(10%) : $ {tax}</small></p>
             {/* <p>Total Price: ${total + shipping + Number(tax)}</p> */}
             <p>Total Price: $ {grandTotal}</p>
+            <br />
+                {
+                    props.children
+                }
+            {/* <button className="review-button">Review Order</button> */}
+
         </div>
     );
 };
